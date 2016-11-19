106cba7e4ec569f2cfbba25b43a97914601a2797
18814127356 <1282379904@qq.com>
2016-11-20 00:01:52 +0800

    初始数据sql

---
+++ doc/sql/init-data.sql
@@ -0,0 +1,21 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50540
+Source Host           : localhost:3306
+Source Database       : sharing
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2016-11-20 00:00:19
+*/
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('0', '超级管理员', '2016-10-29 21:37:15', '\0');
+INSERT INTO `role` VALUES ('1', '管理员', '2016-10-29 21:38:32', '\0');
+INSERT INTO `role` VALUES ('2', '普通用户', '2016-10-29 22:33:02', '\0');
